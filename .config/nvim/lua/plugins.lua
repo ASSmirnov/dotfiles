fb6f7452db5bb3c7d440784a3e0081335aae5518
Anton Smirnov <anton.smirnov@workday.com>
2023-02-09 16:08:59 +0100

    Add coc

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,19 +1,40 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
+  -- Package manager
   use 'wbthomason/packer.nvim'
+  
+  -- Status line
   use {
     'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    requires = {'kyazdani42/nvim-web-devicons', opt = true }
   }
+
+  -- File Explorer
   use {
     'nvim-tree/nvim-tree.lua',
     requires = {
-    'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        'nvim-tree/nvim-web-devicons', -- optional, for file icons
     },
   }	
+
   -- Buffers line
   use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+  
+
+  -- Syntax highlight
+  use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+  }
+
+  -- Color schemes
+  use { "ellisonleao/gruvbox.nvim" }
+  
+  -- Languages support
+  use { 'neoclide/coc.nvim', branch='release' } 
 end)
 
